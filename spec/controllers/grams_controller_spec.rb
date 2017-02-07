86bbe296ed7164ea0f524217f46be62ecfca73f7
Amanda Mark <amarkpark@gmail.com>
2017-02-07 03:33:36 +0000

    Lesson 10 about half

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  let(:user1) {User.create(
+      email:                 'fakeuser@gmail.com',
+      password:              'secretPassword',
+      password_confirmation: 'secretPassword'
+    )}
+
   describe "grams#index action" do
     it "successfully loads the page" do
       get :index
@@ -10,6 +16,7 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#new action" do
     it "successfully loads new message form" do
+      sign_in user1
       get :new
       expect(response).to have_http_status(200)
     end
@@ -17,6 +24,7 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#create action" do
     it "successfully creates a gram and saves to DB" do
+      sign_in user1
       post :create, gram: {message: "Test gram check check"}
       expect(response).to redirect_to root_path
       gram = Gram.last
@@ -24,9 +32,11 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "will not process invalid entries" do
+      sign_in user1
+      precount = Gram.count
       post :create, gram: {message: '' }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
+      expect(Gram.count).to eq(precount)
     end
   end
 end
